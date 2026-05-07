da4685158a331369df05b852f1bf850b9ef10547
pochat <48400262+pochat@users.noreply.github.com>
2026-05-07 15:15:45 -0700

    commit

---
+++ src/Veil.jsx
@@ -0,0 +1,44 @@
+// import { Routes, Route, Link } from "react-router-dom"
+// import { useEffect, useState } from 'react'
+
+function Welcome() {
+
+    return(
+
+        <div className="container">
+
+            <div className="home-title">
+                <h1>Rich or Poor.</h1>
+                <h1>One hidden life.</h1>
+            </div>
+
+            <p className="home-subtitle">
+                You can create the perfect world, but it will impact your life.            
+            </p>
+
+            <button className="animated-button-capsule">
+
+                    Start
+
+            </button>
+
+        </div>
+    )
+}
+
+
+function MyApp() {
+
+    return(
+
+
+        <div>
+
+            <Welcome />
+
+
+        </div>
+    )
+}
+
+export default MyApp
